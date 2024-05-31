d3f21224bf7b9cccbf40d85fd8add024c2a354d4
Cole Essig <hoopwalker@yahoo.com>
2024-05-30 20:05:25 -0700

    feat/updated photos to better reflect site, polished css

--- scripts/index.js
+++ scripts/index.js
@@ -1,32 +1,32 @@
 const initialCards = [
   {
     title: "Yosemite Valley",
-    link: "../images/yosemite-valley.jpg",
+    link: "https://images.unsplash.com/photo-1516687401797-25297ff1462c?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8eW9zZW1pdGUlMjBuYXRpb25hbCUyMHBhcmt8ZW58MHx8MHx8fDA%3D",
   },
 
   {
     title: "Lake Louise",
-    link: "../images/lake-louise.png",
+    link: "https://images.unsplash.com/photo-1539667547529-84c607280d20?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8bGFrZSUyMGxvdWlzZXxlbnwwfHwwfHx8MA%3D%3D",
   },
 
   {
     title: "Bald Mountains",
-    link: "../images/bald-mountains.png",
+    link: "https://images.unsplash.com/photo-1601025252036-36faf914679d?q=80&w=1858&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
 
   {
     title: "Latemar",
-    link: "../images/latemar.png",
+    link: "https://images.unsplash.com/photo-1536431311719-398b6704d4cc?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGxhdGVtYXJ8ZW58MHx8MHx8fDA%3D",
   },
 
   {
     title: "Vanoise National Park",
-    link: "../images/vanoise-national-park.png",
+    link: "https://images.unsplash.com/photo-1650533139610-0f4f5c0c34bf?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dmFub2lzZSUyMG5hdGlvbmFsJTIwcGFya3xlbnwwfHwwfHx8MA%3D%3D",
   },
 
   {
     title: "Lago di Braies",
-    link: "../images/lago-di-braies.png",
+    link: "https://images.unsplash.com/photo-1601893920895-e3ed4a655d27?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bGFnbyUyMGRpJTIwYnJhaWVzfGVufDB8fDB8fHww",
   },
 ];
 

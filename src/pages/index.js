cf74563549a9ba369e53c5b922cce9ae32b0e689
Cole Essig <hoopwalker@yahoo.com>
2024-07-27 18:14:09 -0700

    added webpack and all adjacent files to function, resctructured accordingly

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,48 +1,52 @@
+import {
+  initialCards,
+  validationSettings,
+  cardData,
+} from "../utils/constants.js";
+import "../pages/index.css";
 import Card from "../components/Card.js";
 import FormValidator from "../components/FormValidator.js";
-import Modal from "../components/Modal.js";
-import PopupWithForm from "../components/ModalWithForm.js";
 import ModalWithForm from "../components/ModalWithForm.js";
 import ModalWithImage from "../components/ModalWithImage.js";
 import Section from "../components/Section.js";
 import UserInfo from "../components/UserInfo.js";
 
-const initialCards = [
-  {
-    title: "Yosemite Valley",
-    link: "https://images.unsplash.com/photo-1516687401797-25297ff1462c?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8eW9zZW1pdGUlMjBuYXRpb25hbCUyMHBhcmt8ZW58MHx8MHx8fDA%3D",
-  },
+// const initialCards = [
+//   {
+//     title: "Yosemite Valley",
+//     link: "https://images.unsplash.com/photo-1516687401797-25297ff1462c?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8eW9zZW1pdGUlMjBuYXRpb25hbCUyMHBhcmt8ZW58MHx8MHx8fDA%3D",
+//   },
 
-  {
-    title: "Lake Louise",
-    link: "https://images.unsplash.com/photo-1539667547529-84c607280d20?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8bGFrZSUyMGxvdWlzZXxlbnwwfHwwfHx8MA%3D%3D",
-  },
+//   {
+//     title: "Lake Louise",
+//     link: "https://images.unsplash.com/photo-1539667547529-84c607280d20?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8bGFrZSUyMGxvdWlzZXxlbnwwfHwwfHx8MA%3D%3D",
+//   },
 
-  {
-    title: "Bald Mountains",
-    link: "https://images.unsplash.com/photo-1601025252036-36faf914679d?q=80&w=1858&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  },
+//   {
+//     title: "Bald Mountains",
+//     link: "https://images.unsplash.com/photo-1601025252036-36faf914679d?q=80&w=1858&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+//   },
 
-  {
-    title: "Latemar",
-    link: "https://images.unsplash.com/photo-1536431311719-398b6704d4cc?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGxhdGVtYXJ8ZW58MHx8MHx8fDA%3D",
-  },
+//   {
+//     title: "Latemar",
+//     link: "https://images.unsplash.com/photo-1536431311719-398b6704d4cc?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGxhdGVtYXJ8ZW58MHx8MHx8fDA%3D",
+//   },
 
-  {
-    title: "Vanoise National Park",
-    link: "https://images.unsplash.com/photo-1650533139610-0f4f5c0c34bf?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dmFub2lzZSUyMG5hdGlvbmFsJTIwcGFya3xlbnwwfHwwfHx8MA%3D%3D",
-  },
+//   {
+//     title: "Vanoise National Park",
+//     link: "https://images.unsplash.com/photo-1650533139610-0f4f5c0c34bf?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dmFub2lzZSUyMG5hdGlvbmFsJTIwcGFya3xlbnwwfHwwfHx8MA%3D%3D",
+//   },
 
-  {
-    title: "Lago di Braies",
-    link: "https://images.unsplash.com/photo-1601893920895-e3ed4a655d27?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bGFnbyUyMGRpJTIwYnJhaWVzfGVufDB8fDB8fHww",
-  },
-];
+//   {
+//     title: "Lago di Braies",
+//     link: "https://images.unsplash.com/photo-1601893920895-e3ed4a655d27?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bGFnbyUyMGRpJTIwYnJhaWVzfGVufDB8fDB8fHww",
+//   },
+// ];
 
-const cardData = {
-  title: "Yosemite Valley",
-  link: "https://images.unsplash.com/photo-1516687401797-25297ff1462c?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8eW9zZW1pdGUlMjBuYXRpb25hbCUyMHBhcmt8ZW58MHx8MHx8fDA%3D",
-};
+// const cardData = {
+//   title: "Yosemite Valley",
+//   link: "https://images.unsplash.com/photo-1516687401797-25297ff1462c?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8eW9zZW1pdGUlMjBuYXRpb25hbCUyMHBhcmt8ZW58MHx8MHx8fDA%3D",
+// };
 
 /* ELEMENTS */
 //  PROFILE ELEMENTS
@@ -158,14 +162,14 @@ function makeCard(cardData) {
 
 /* Validation */
 
-const validationSettings = {
-  formSelector: ".modal__form",
-  inputSelector: ".modal__input",
-  submitButtonSelector: ".modal__button",
-  inactiveButtonClass: "modal__button_disabled",
-  inputErrorClass: "modal__input_type_error",
-  errorClass: "modal__error_visible",
-};
+// const validationSettings = {
+//   formSelector: ".modal__form",
+//   inputSelector: ".modal__input",
+//   submitButtonSelector: ".modal__button",
+//   inactiveButtonClass: "modal__button_disabled",
+//   inputErrorClass: "modal__input_type_error",
+//   errorClass: "modal__error_visible",
+// };
 const editFormValidator = new FormValidator(
   validationSettings,
   profileEditForm
